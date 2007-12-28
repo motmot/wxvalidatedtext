233e5a516de994c8f82cb32267521950d6718d11
astraw <astraw@9608957c-362e-0410-90bb-ddb82fccb57f>
2007-12-28 23:07:05 +0000

    update to namespace package
    git-svn-id: https://code.astraw.com/motmot/trunk/wxvalidatedtext@330 9608957c-362e-0410-90bb-ddb82fccb57f

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 import os, sys
 
-setup(name='wxvalidatedtext',
+setup(name='motmot.wxvalidatedtext',
       description='validated integer/float text entry field for wxPython',
       author='Andrew Straw',
       author_email='strawman@astraw.com',
@@ -10,11 +10,12 @@ setup(name='wxvalidatedtext',
       version='0.5.1', # keep in sync with wxvalidatedtext/wxvalidatedtext.py
       zip_safe=True,
 
-      packages = ['wxvalidatedtext'],
+      namespace_packages = ['motmot'],
+      packages = find_packages(),
       entry_points = {'gui_scripts': [
-    'wxvalidatedtext_demo = wxvalidatedtext.demo:main',
+    'wxvalidatedtext_demo = motmot.wxvalidatedtext.demo:main',
     ]
                       },
       package_data = {'wxvalidatedtext_demo':['demo.xrc']},
-      eager_resources = ['wxvalidatedtext/demo.xrc'],
+      eager_resources = ['motmot/wxvalidatedtext/demo.xrc'],
       )
